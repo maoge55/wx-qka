2d207ba50b3d91fb4c857db821e2105b51934bf2
maoge <1126642524@qq.com>
2022-04-13 11:47:21 +0800

    第一次提交

---
+++ config/config.js
@@ -0,0 +1,293 @@
+const joiners=[
+  {
+    "_id": "41ae62ef622f43ce0c58d53250322cd7",
+    "_openid": "oso3G4jwja6FdWFSPaY_SqUvZ9FE",
+    "admin": false,
+    "jf": 0,
+    "joinner": true,
+    "jonnerName": "毛哥8",
+    "time": 1647264718702,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/2fcKHZ0H9nQW7tA78XHJJIPsj6cxYWfLCbdXrSDMuRHTgtr3fGMvsWdDkLKjvWTuYUPokx5ABldDJDElr0KMhA/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "独孤梦",
+      "province": ""
+    }
+  },
+  {
+    "_id": "17e3426e622f3df41377c66c45a361f4",
+    "_openid": "oso3G4jwja6FdWFSPaY_SqUvZ9FE0",
+    "admin": false,
+    "jf": 20,
+    "joinner": true,
+    "jonnerName": "mm",
+    "time": 1647263260016,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/2fcKHZ0H9nQW7tA78XHJJIPsj6cxYWfLCbdXrSDMuRHTgtr3fGMvsWdDkLKjvWTuYUPokx5ABldDJDElr0KMhA/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "独孤梦",
+      "province": ""
+    }
+  },
+  {
+    "_id": "41ae62ef622f437d0c58c76300d8f0d8",
+    "_openid": "oso3G4q-LGygAD5azTGjBTgcBRs8",
+    "admin": false,
+    "jf": 0,
+    "joinner": true,
+    "jonnerName": "毛哥7",
+    "time": 1647264637336,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/4Y56GQmBAhSPFPyY6hRlJPLaN5Jl7zLQfGjU4pdib6pfwc1Wr56ic29eNcibED6HTeViaOnMyyE1ib97Lz7sWSmuzRg/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "鬼魂七",
+      "province": ""
+    }
+  },
+  {
+    "_id": "41ae62ef622f39e00c56bbc36b352b58",
+    "_openid": "oso3G4q-LGygAD5azTGjBTgcBRs80",
+    "admin": false,
+    "jf": 100,
+    "joinner": true,
+    "jonnerName": "毛哥",
+    "time": 1647264346059,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/4Y56GQmBAhSPFPyY6hRlJPLaN5Jl7zLQfGjU4pdib6pfwc1Wr56ic29eNcibED6HTeViaOnMyyE1ib97Lz7sWSmuzRg/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "鬼魂七",
+      "province": ""
+    }
+  },
+  {
+    "_id": "bf4a0bf2622f42b31488b18026a8c23a",
+    "_openid": "oso3G4q-LGygAD5azTGjBTgcBRs81",
+    "admin": false,
+    "jf": 0,
+    "joinner": true,
+    "jonnerName": "毛哥1",
+    "time": 1647264435581,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/4Y56GQmBAhSPFPyY6hRlJPLaN5Jl7zLQfGjU4pdib6pfwc1Wr56ic29eNcibED6HTeViaOnMyyE1ib97Lz7sWSmuzRg/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "鬼魂七",
+      "province": ""
+    }
+  },
+  {
+    "_id": "41ae62ef622f430b0c58ab324af638eb",
+    "_openid": "oso3G4q-LGygAD5azTGjBTgcBRs82",
+    "admin": false,
+    "jf": 0,
+    "joinner": true,
+    "jonnerName": "毛哥2",
+    "time": 1647264523058,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/4Y56GQmBAhSPFPyY6hRlJPLaN5Jl7zLQfGjU4pdib6pfwc1Wr56ic29eNcibED6HTeViaOnMyyE1ib97Lz7sWSmuzRg/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "鬼魂七",
+      "province": ""
+    }
+  },
+  {
+    "_id": "41ae62ef622f432e0c58b2eb1f65a771",
+    "_openid": "oso3G4q-LGygAD5azTGjBTgcBRs85",
+    "admin": false,
+    "jf": 0,
+    "joinner": true,
+    "jonnerName": "毛哥4",
+    "time": 1647264558466,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/4Y56GQmBAhSPFPyY6hRlJPLaN5Jl7zLQfGjU4pdib6pfwc1Wr56ic29eNcibED6HTeViaOnMyyE1ib97Lz7sWSmuzRg/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "鬼魂七",
+      "province": ""
+    }
+  },
+  {
+    "_id": "41ae62ef622f43500c58b741349758bc",
+    "_openid": "oso3G4q-LGygAD5azTGjBTgcBRs86",
+    "admin": false,
+    "jf": 0,
+    "joinner": true,
+    "jonnerName": "毛哥6",
+    "time": 1647264592191,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/4Y56GQmBAhSPFPyY6hRlJPLaN5Jl7zLQfGjU4pdib6pfwc1Wr56ic29eNcibED6HTeViaOnMyyE1ib97Lz7sWSmuzRg/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "鬼魂七",
+      "province": ""
+    }
+  },{
+    "_id": "41ae62ef622f43ce0c58d53250322cd7",
+    "_openid": "oso3G4jwja6FdWFSPaY_SqUvZ9FE",
+    "admin": false,
+    "jf": 0,
+    "joinner": true,
+    "jonnerName": "毛哥8",
+    "time": 1647264718702,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/2fcKHZ0H9nQW7tA78XHJJIPsj6cxYWfLCbdXrSDMuRHTgtr3fGMvsWdDkLKjvWTuYUPokx5ABldDJDElr0KMhA/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "独孤梦",
+      "province": ""
+    }
+  },
+  {
+    "_id": "17e3426e622f3df41377c66c45a361f4",
+    "_openid": "oso3G4jwja6FdWFSPaY_SqUvZ9FE0",
+    "admin": false,
+    "jf": 20,
+    "joinner": true,
+    "jonnerName": "mm",
+    "time": 1647263260016,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/2fcKHZ0H9nQW7tA78XHJJIPsj6cxYWfLCbdXrSDMuRHTgtr3fGMvsWdDkLKjvWTuYUPokx5ABldDJDElr0KMhA/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "独孤梦",
+      "province": ""
+    }
+  },
+  {
+    "_id": "41ae62ef622f437d0c58c76300d8f0d8",
+    "_openid": "oso3G4q-LGygAD5azTGjBTgcBRs8",
+    "admin": false,
+    "jf": 0,
+    "joinner": true,
+    "jonnerName": "毛哥7",
+    "time": 1647264637336,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/4Y56GQmBAhSPFPyY6hRlJPLaN5Jl7zLQfGjU4pdib6pfwc1Wr56ic29eNcibED6HTeViaOnMyyE1ib97Lz7sWSmuzRg/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "鬼魂七",
+      "province": ""
+    }
+  },
+  {
+    "_id": "41ae62ef622f39e00c56bbc36b352b58",
+    "_openid": "oso3G4q-LGygAD5azTGjBTgcBRs80",
+    "admin": false,
+    "jf": 100,
+    "joinner": true,
+    "jonnerName": "毛哥",
+    "time": 1647264346059,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/4Y56GQmBAhSPFPyY6hRlJPLaN5Jl7zLQfGjU4pdib6pfwc1Wr56ic29eNcibED6HTeViaOnMyyE1ib97Lz7sWSmuzRg/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "鬼魂七",
+      "province": ""
+    }
+  },
+  {
+    "_id": "bf4a0bf2622f42b31488b18026a8c23a",
+    "_openid": "oso3G4q-LGygAD5azTGjBTgcBRs81",
+    "admin": false,
+    "jf": 0,
+    "joinner": true,
+    "jonnerName": "毛哥1",
+    "time": 1647264435581,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/4Y56GQmBAhSPFPyY6hRlJPLaN5Jl7zLQfGjU4pdib6pfwc1Wr56ic29eNcibED6HTeViaOnMyyE1ib97Lz7sWSmuzRg/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "鬼魂七",
+      "province": ""
+    }
+  },
+  {
+    "_id": "41ae62ef622f430b0c58ab324af638eb",
+    "_openid": "oso3G4q-LGygAD5azTGjBTgcBRs82",
+    "admin": false,
+    "jf": 0,
+    "joinner": true,
+    "jonnerName": "毛哥2",
+    "time": 1647264523058,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/4Y56GQmBAhSPFPyY6hRlJPLaN5Jl7zLQfGjU4pdib6pfwc1Wr56ic29eNcibED6HTeViaOnMyyE1ib97Lz7sWSmuzRg/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "鬼魂七",
+      "province": ""
+    }
+  },
+  {
+    "_id": "41ae62ef622f432e0c58b2eb1f65a771",
+    "_openid": "oso3G4q-LGygAD5azTGjBTgcBRs85",
+    "admin": false,
+    "jf": 0,
+    "joinner": true,
+    "jonnerName": "毛哥4",
+    "time": 1647264558466,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/4Y56GQmBAhSPFPyY6hRlJPLaN5Jl7zLQfGjU4pdib6pfwc1Wr56ic29eNcibED6HTeViaOnMyyE1ib97Lz7sWSmuzRg/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "鬼魂七",
+      "province": ""
+    }
+  },
+  {
+    "_id": "41ae62ef622f43500c58b741349758bc",
+    "_openid": "oso3G4q-LGygAD5azTGjBTgcBRs86",
+    "admin": false,
+    "jf": 0,
+    "joinner": true,
+    "jonnerName": "毛哥6",
+    "time": 1647264592191,
+    "userWxInfo": {
+      "avatarUrl": "https://thirdwx.qlogo.cn/mmopen/vi_32/4Y56GQmBAhSPFPyY6hRlJPLaN5Jl7zLQfGjU4pdib6pfwc1Wr56ic29eNcibED6HTeViaOnMyyE1ib97Lz7sWSmuzRg/132",
+      "city": "",
+      "country": "",
+      "gender": 0,
+      "language": "zh_CN",
+      "nickName": "鬼魂七",
+      "province": ""
+    }
+  }
+]
+
+module.exports = {
+  joiners
+}
---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-3gu31qy1baa50fbc","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
